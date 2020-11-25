49e2fb2b3830d26c49ebcb5a9e6caab9231aca6c
Cheerala Prathika <cheeralaprathika02@gmail.com>
2020-11-25 11:24:42 +0530

    Added doc on Project Domain

--- src/main/java/com/cg/ppmtoolapi/domain/Project.java
+++ src/main/java/com/cg/ppmtoolapi/domain/Project.java
@@ -1,5 +1,12 @@
 package com.cg.ppmtoolapi.domain;
 
+/* 
+ * Project Domain is used as data traveller object.
+ * All Project Validation will be performed here.
+ * 
+ * @author prathika
+ */
+
 public class Project {
 
 }
